9a0f6f12f5cf2c90eda04bb1c09ca229a9486408
MASTAN <shaikm496@gmail.comgit confog --global  --listgit config  --global user.email shaikm496@gmail.com>
2025-12-30 15:11:18 +0530

    seconed update to python project

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-print("Hello ! this is my first vs code project")
-name= "mastan"
-print("hello",name)
+print("Hello! this is my first vs code project")
+
+name = "mastan"
+print("hello", name)
+
 print("welcome to python learning")
+print("this is my second git push")
